de59d7d22798b25076ad1bbc1b03fd49880e340f
Alija Fajić <alee.fajic@hotmail.com>
2021-04-29 22:55:40 +0200

    Creating components

---
+++ cocktail-list/src/Components/Navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navbar() {
+  return <div>Nav</div>;
+}
+
+export default Navbar;
---
+++ cocktail-list/src/Pages/SingleCocktail.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SingleCocktail() {
+  return <div>Cocktail</div>;
+}
+
+export default SingleCocktail;
